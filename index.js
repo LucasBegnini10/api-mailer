cffe85ae976766cf8bc3020c5dff346086f795da
LucasBegnini10 <begninilucas12@gmail.com>
2023-02-18 14:04:31 -0300

    add serverless

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   optionsSuccessStatus: 200,
-}
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.listen(port, () => console.log(`API ON ==> PORT ====> ` + port))
-app.post('/send', main)
+app.listen(port, () => console.log(`API ON ==> PORT ====> ` + port));
+app.post("/send", main);
 
 async function main(req, res) {
-    const {body} = req
+  const { body } = req;
 
-    const from = body?.from
-    const subject = body?.subject
-    const text = body?.text
-    const to  = body?.to
+  const from = body?.from;
+  const subject = body?.subject;
+  const text = body?.text;
+  const to = body?.to;
 
-    if(!from) {
-        return res.status(400).send("Está faltando o campo 'from'")
-    }
-    if(!subject) {
-        return res.status(400).send("Está faltando o campo 'subject'")
-    }
-    if(!text) {
-        return res.status(400).send("Está faltando o campo 'text'")
-    }
-    if(!to) {
-        return res.status(400).send("Está faltando o campo 'to'")
-    }
+  if (!from) {
+    return res.status(400).send("Está faltando o campo 'from'");
+  }
+  if (!subject) {
+    return res.status(400).send("Está faltando o campo 'subject'");
+  }
+  if (!text) {
+    return res.status(400).send("Está faltando o campo 'text'");
+  }
+  if (!to) {
+    return res.status(400).send("Está faltando o campo 'to'");
+  }
 
-  let transporter = nodemailer.createTransport({ 
-    host: 'smtp.gmail.com',
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
-    auth: { 
-       user: process.env.USER_AUTH, 
-       pass: process.env.PASSWORD_AUTH 
-     } 
-    });
+    auth: {
+      user: process.env.USER_AUTH,
+      pass: process.env.PASSWORD_AUTH,
+    },
+  });
 
   let info = await transporter.sendMail({
     from,
     to,
-    subject, 
-    text, 
+    subject,
+    text,
   });
 
-
-  res.json({info})
+  res.json({ info });
 }
+
+module.exports.handler = serverless(app);
